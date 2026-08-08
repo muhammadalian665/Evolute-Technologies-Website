83032753e30510b013f38035a308b832bfc60c3c
muhammadalian665 <muhammadalian665@gmail.com>
2026-08-09 03:48:06 +0500

    Add software hiring section and update services styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-
 import IntroSection from "./components/IntroSection";
 import ClientsSection from "./components/ClientsSection";
 import Services from "./components/Services";
-
+import SoftwareHiring from "./components/SoftwareHiring";
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
-    
       <IntroSection />
       <ClientsSection />
-
       <Services />
+       <SoftwareHiring />
 
 
     </>
