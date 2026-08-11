da7f6d5d2c3e752d5301f3f90d8a455b25241aa0
muhammadalian665 <muhammadalian665@gmail.com>
2026-08-11 17:36:08 +0500

    Connected contact us page with Hire Us button in NavBar (Ref : Navbar )

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,62 +1,70 @@
 import "./Navbar.css";
 import Logo from "../../assets/logo.png";
-import { Link } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
 
 function Navbar() {
-  return (
-    <header className="navbar">
+    const navigate = useNavigate();
+
+    return (
+        <header className="navbar">
+
+            {/* Logo */}
+            <div className="logo">
+                <Link to="/">
+                    <img
+                        src={Logo}
+                        alt="Evolute Technologies"
+                    />
+                </Link>
+            </div>
 
 
-      {/* Logo */}
-      <div className="logo">
-        <Link to="/">
-          <img src={Logo} alt="Evolute Technologies" />
-        </Link>
-      </div>
+            {/* Navigation Links */}
+            <ul className="nav-links">
+
+                <li>
+                    <Link to="/">
+                        Home
+                    </Link>
+                </li>
+
+                <li>
+                    <Link to="/about">
+                        About Us
+                    </Link>
+                </li>
+
+                <li>
+                    <Link to="/services">
+                        Services
+                    </Link>
+                </li>
+
+                <li>
+                    <Link to="/case-studies">
+                        Case Studies
+                    </Link>
+                </li>
+
+                <li>
+                    <Link to="/contact-us">
+                        Contact Us
+                    </Link>
+                </li>
+
+            </ul>
 
 
-      {/* Navigation Links */}
-      <ul className="nav-links">
+            {/* Hire Button */}
+            <button
+                className="hire-btn"
+                onClick={() => navigate("/contact-us")}
+            >
+                Hire Us
+            </button>
 
-
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-
-
-        <li>
-          <Link to="/about">About Us</Link>
-        </li>
-
-
-        <li>
-          <Link to="/services">Services</Link>
-        </li>
-
-
-        <li>
-          <Link to="/case-studies">Case Studies</Link>
-        </li>
-
-
-        <li>
-          <Link to="/contact-us">Contact Us</Link>
-        </li>
-
-
-      </ul>
-
-
-      {/* Hire Button */}
-      <button className="hire-btn">
-        Hire Us
-      </button>
-
-
-    </header>
-  );
+        </header>
+    );
 }
-
 
 export default Navbar;
