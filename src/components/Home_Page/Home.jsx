77ae1b512f687997be0c51fa6503addc40485b07
muhammadalian665 <muhammadalian665@gmail.com>
2026-08-12 17:39:24 +0500

    Added circular wheel caursel

--- src/components/Home_Page/Home.jsx
+++ src/components/Home_Page/Home.jsx
@@ -1,24 +1,34 @@
 import Hero from "./Hero/Hero";
 import IntroSection from "./IntroSection/IntroSection";
-import ClientsSection from "./ClientsSection/ClientsSection";
 import Services from "./Services/Services";
+import CircularGallery from "./Circular_Gallery/Circular_Gallery";
+import ClientsSection from "./ClientsSection/ClientsSection";
 import SoftwareHiring from "./SoftwareHiring/SoftwareHiring";
 import GrowthHiring from "./GrowthHiring/GrowthHiring";
 import Philosphy from "./Philosphy/Philosphy";
 
 function Home() {
-  return (
-    <>
-      <Hero />
-      <IntroSection />
-      
-      <Services />
-      <ClientsSection />
-      <SoftwareHiring />
-      <GrowthHiring />
-      <Philosphy />
-    </>
-  );
+    return (
+        <>
+            <Hero />
+
+            <IntroSection />
+
+            <Services />
+
+            <CircularGallery />
+
+            <ClientsSection />
+
+            <SoftwareHiring />
+
+            <GrowthHiring />
+
+            <Philosphy />
+
+            <ClientsSection />
+        </>
+    );
 }
 
 export default Home;
