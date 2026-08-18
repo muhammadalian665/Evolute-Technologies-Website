3c3804ceae9ea1fe49d6e5ca101dfe15f6f1ec00
muhammadalian665 <muhammadalian665@gmail.com>
2026-08-18 19:55:07 +0500

    made Case study header

--- src/components/Software_Tech_Case_Studies/Software_Case_Studies.jsx
+++ src/components/Software_Tech_Case_Studies/Software_Case_Studies.jsx
@@ -1,38 +1,11 @@
+import "./Software_Case_Studies.css";
+import SoftwareCaseStudyHeader from "./Software_Case_Study_Components/SoftwareCaseStudyHeader";
+
 function Software_Case_Studies() {
     return (
-        <section
-            style={{
-                minHeight: "80vh",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                flexDirection: "column",
-                padding: "80px 20px",
-                background: "#ffffff",
-                textAlign: "center",
-            }}
-        >
-            <h1
-                style={{
-                    fontSize: "48px",
-                    color: "#0950CD",
-                    marginBottom: "20px",
-                }}
-            >
-                Software & Tech Case Studies
-            </h1>
-
-            <p
-                style={{
-                    fontSize: "18px",
-                    color: "#667085",
-                    maxWidth: "600px",
-                }}
-            >
-                Routing is working successfully.
-                This is the Software & Tech Case Studies page.
-            </p>
-        </section>
+        <>
+            <SoftwareCaseStudyHeader />
+        </>
     );
 }
 
---
+++ src/components/Software_Tech_Case_Studies/Software_Case_Study_Components/SoftwareCaseStudyHeader.jsx
@@ -0,0 +1,17 @@
+import "./SoftwareCaseStudyHeader.css";
+
+function SoftwareCaseStudyHeader() {
+    return (
+        <section className="software-case-study-header">
+            <div className="software-case-study-header-content">
+                
+
+                <h1>Case Studies</h1>
+
+                <div className="software-case-study-line"></div>
+            </div>
+        </section>
+    );
+}
+
+export default SoftwareCaseStudyHeader;
