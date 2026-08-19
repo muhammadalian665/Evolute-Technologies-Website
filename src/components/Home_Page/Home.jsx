8e52140260caa99814a0321a94e1f0f2ff5e53d5
muhammadalian665 <muhammadalian665@gmail.com>
2026-08-19 21:18:38 +0500

    Semi Final

--- src/components/Home_Page/Home.jsx
+++ src/components/Home_Page/Home.jsx
@@ -1,30 +1,10 @@
 import Hero from "./Hero/Hero";
-import IntroSection from "./IntroSection/IntroSection";
-import Services from "./Services/Services";
-import CircularGallery from "./Circular_Gallery/Circular_Gallery";
-import ClientsSection from "./ClientsSection/ClientsSection";
-import SoftwareHiring from "./SoftwareHiring/SoftwareHiring";
-import GrowthHiring from "./GrowthHiring/GrowthHiring";
-import Philosphy from "./Philosphy/Philosphy";
+
 
 function Home() {
     return (
         <>
             <Hero />
-
-            <IntroSection />
-
-            <Services />
-
-            <CircularGallery />
-
-            <ClientsSection />
-
-            <SoftwareHiring />
-
-            <GrowthHiring />
-
-            <Philosphy />
 
         </>
     );
