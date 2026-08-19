9a5fdb47c39ad4aa9951e6f793355095bb8088c8
muhammadalian665 <muhammadalian665@gmail.com>
2026-08-19 21:44:01 +0500

    CTA Correction

--- src/components/About_Us_Page/ServiceCTA/ServiceCTA.jsx
+++ src/components/About_Us_Page/ServiceCTA/ServiceCTA.jsx
@@ -1,15 +1,32 @@
+import { useNavigate } from "react-router-dom";
 import "./ServiceCTA.css";
 
 function ServiceCTA() {
+
+  const navigate = useNavigate();
+
+  const handlePortfolioClick = () => {
+    navigate("/case-studies");
+  };
+
   return (
     <section className="service-cta">
-      <div className="service-cta-content">
-        <h2>Ready To Explore Our Services & Client Stories?</h2>
 
-        <button className="service-cta-button">
+      <div className="service-cta-content">
+
+        <h2>
+          Ready To Explore Our Services & Client Stories?
+        </h2>
+
+        <button
+          className="service-cta-button"
+          onClick={handlePortfolioClick}
+        >
           Dive Into Our Portfolio
         </button>
+
       </div>
+
     </section>
   );
 }
